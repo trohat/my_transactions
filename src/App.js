841bcbc16b3b93757fe093f48bedd205800a5091
Trohat <petr.sed@centrum.cz>
2018-10-03 19:56:28 +0200

    Basic app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -17,8 +15,9 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Go to React
           </a>
+          <a href="https://git-scm.com/">Go to GIT</a>
         </header>
       </div>
     );
